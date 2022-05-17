85204e019ffbe9fceef37afef54f1013a9e3e9e4
inkognitro <contact@maecefischer.ch>
2022-05-17 09:53:05 +0200

    move packages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
-import {
-  anonymousAuthUser,
-  AuthUser,
-  BrowserCurrentUserRepository,
-  CurrentUserProvider,
-  CurrentUserRepositoryProvider,
-} from "../packages/core/auth";
 
 function App() {
-  const [currentUserState, setCurrentUserState] = useState<AuthUser>(anonymousAuthUser);
-  const browserCurrentUserRepositoryRef = useRef(new BrowserCurrentUserRepository(setCurrentUserState));
   return (
-      <CurrentUserRepositoryProvider value={browserCurrentUserRepositoryRef.current}>
-        <CurrentUserProvider value={currentUserState}>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
-        </CurrentUserProvider>
-      </CurrentUserRepositoryProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
---
+++ src/ServiceProvider.tsx
@@ -0,0 +1,20 @@
+import React, { FC, PropsWithChildren, useRef, useState } from "react";
+import {
+    anonymousAuthUser,
+    AuthUser,
+    BrowserCurrentUserRepository,
+    CurrentUserProvider,
+    CurrentUserRepositoryProvider
+} from './packages/core/auth';
+
+export const ServiceProvider: FC<PropsWithChildren<{}>> = props => {
+    const [currentUserState, setCurrentUserState] = useState<AuthUser>(anonymousAuthUser);
+    const browserCurrentUserRepositoryRef = useRef(new BrowserCurrentUserRepository(setCurrentUserState));
+    return (
+        <CurrentUserRepositoryProvider value={browserCurrentUserRepositoryRef.current}>
+            <CurrentUserProvider value={currentUserState}>
+                {props.children}
+            </CurrentUserProvider>
+        </CurrentUserRepositoryProvider>
+    );
+};
