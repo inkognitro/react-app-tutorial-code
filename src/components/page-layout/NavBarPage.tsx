b996c374c6148dcb77222bc5f5bc3a8d617f9917
inkognitro <contact@maecefischer.ch>
2022-05-23 12:26:15 +0200

    implement links

--- src/components/page-layout/NavBarPage.tsx
+++ src/components/page-layout/NavBarPage.tsx
@@ -1,11 +1,14 @@
 import React, { FC, MouseEvent } from 'react';
 import { BlankPage, BlankPageProps } from './BlankPage';
 import { anonymousAuthUser, useCurrentUser, useCurrentUserRepository } from '@packages/core/auth';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Nav() {
+    const navigate = useNavigate();
     const currentUserRepo = useCurrentUserRepository();
     const currentUser = useCurrentUser();
     const isLoggedIn = currentUser.type === 'authenticated';
+    const currentUserDisplayName = currentUser.type === 'authenticated' ? currentUser.data.username : 'Anonymous';
     function loginUser(event: MouseEvent<HTMLAnchorElement>) {
         event.preventDefault();
         currentUserRepo.setCurrentUser({
@@ -20,6 +23,7 @@ function Nav() {
     function logoutUser(event: MouseEvent<HTMLAnchorElement>) {
         event.preventDefault();
         currentUserRepo.setCurrentUser(anonymousAuthUser);
+        navigate('/');
     }
     return (
         <div
@@ -29,17 +33,22 @@ function Nav() {
                 width: '600px',
                 textAlign: 'center',
             }}>
+            <Link to="/">Home</Link> &ndash;{' '}
             {!isLoggedIn && (
-                <a href="#" onClick={loginUser}>
-                    login
-                </a>
+                <>
+                    <Link to="/auth/register">Register</Link> &ndash;{' '}
+                    <a href="#" onClick={loginUser}>
+                        Login
+                    </a>
+                </>
             )}
             {isLoggedIn && (
                 <a href="#" onClick={logoutUser}>
-                    logout
+                    Logout
                 </a>
-            )}
-            :: {currentUser.type === 'authenticated' ? currentUser.data.username : 'Anonymous'}
+            )}{' '}
+            :: {isLoggedIn && <Link to="/user-management/my-settings">{currentUserDisplayName}</Link>}
+            {!isLoggedIn && currentUserDisplayName}
         </div>
     );
 }
