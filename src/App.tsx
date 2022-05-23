584facbfc40ab526080b9bfc3c11ebea4c4babe1
inkognitro <contact@maecefischer.ch>
2022-05-23 11:53:56 +0200

    add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { IndexPage } from '@pages/IndexPage';
 import { RegisterPage } from '@pages/auth/RegisterPage';
 import { MySettingsPage } from '@pages/user-management/MySettingsPage';
+import { NotFoundPage } from '@pages/NotFoundPage';
 
 function AppRoutes() {
     const currentUser = useCurrentUser();
@@ -13,6 +14,7 @@ function AppRoutes() {
             <Route path="/" element={<IndexPage />} />
             <Route path="/auth/register" element={<RegisterPage />} />
             {isUserLoggedIn && <Route path="/user-management/my-settings" element={<MySettingsPage />} />}
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
 }
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { NavBarPage } from '@components/page-layout';
+
+export const NotFoundPage: FC = () => {
+    return <NavBarPage title="Not Found">Not Found.</NavBarPage>;
+};
