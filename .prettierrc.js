6eed3f1ce3849fc916190b3e231b57562e512c4f
inkognitro <contact@maecefischer.ch>
2022-05-31 14:34:57 +0200

    fix prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     arrowParens: 'always',
     bracketSpacing: true,
-    jsxBracketSameLine: true,
+    bracketSameLine: true,
     jsxSingleQuote: false,
     printWidth: 120,
     quoteProps: 'as-needed',
