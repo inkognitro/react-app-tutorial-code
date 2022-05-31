9eb03fb50634d775a6b692fc674686c6c036eac6
inkognitro <contact@maecefischer.ch>
2022-05-31 18:35:52 +0200

    implement language switch

--- src/ServiceProvider.tsx
+++ src/ServiceProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren, useEffect, useRef, useState } from 'react';
+import React, { FC, PropsWithChildren, useRef, useState } from 'react';
 import {
     anonymousAuthUser,
     AuthUser,
@@ -24,15 +24,18 @@ export const ServiceProvider: FC<PropsWithChildren<{}>> = (props) => {
     const [currentUserState, setCurrentUserState] = useState<AuthUser>(anonymousAuthUser);
     const [i18nState, setI18nState] = useState<I18n>(createI18n('en-US'));
     const translatorRef = useRef<DictionaryTranslator>(new DictionaryTranslator(enUS));
-    const i18nManagerRef = useRef<BrowserI18nManager>(new BrowserI18nManager(setI18nState));
-    useEffect(() => {
-        if (i18nState.languageCode === 'en-US') {
-            translatorRef.current.setDictionary(enUS);
-        }
-        if (i18nState.languageCode === 'de-CH') {
-            translatorRef.current.setDictionary(deCH);
-        }
-    }, [i18nState.languageCode]);
+    const i18nManagerRef = useRef<BrowserI18nManager>(
+        new BrowserI18nManager((i18nState) => {
+            switch (i18nState.languageCode) {
+                case 'de-CH':
+                    translatorRef.current.setDictionary(deCH);
+                    break;
+                default:
+                    translatorRef.current.setDictionary(enUS);
+            }
+            setI18nState(i18nState);
+        })
+    );
     const browserCurrentUserRepositoryRef = useRef(new BrowserCurrentUserRepository(setCurrentUserState));
     const configRef = useRef<Config>({
         companyName: 'ACME',
