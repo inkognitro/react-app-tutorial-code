45c18ab849bf977e8e52e04330d3ee0946094417
inkognitro <contact@maecefischer.ch>
2022-06-04 13:40:50 +0200

    provide registration form

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,17 +1,30 @@
 import { FC, useState } from 'react';
 import { NavBarPage } from '@components/page-layout';
 import { useTranslator, T } from '@packages/core/i18n';
-import { Checkbox, CheckboxState, Form, TextField, TextFieldState } from '@packages/core/form';
+import {
+    Checkbox,
+    CheckboxState,
+    createCheckboxState,
+    createTextFieldState,
+    Form,
+    TextField,
+    TextFieldState,
+} from '@packages/core/form';
 import { FunctionalLink } from '@packages/core/routing';
+import { Typography } from '@mui/material';
 
 type RegistrationFormState = {
     usernameField: TextFieldState;
+    emailField: TextFieldState;
+    passwordField: TextFieldState;
     agreeCheckbox: CheckboxState;
 };
 
 function createRegistrationFormState(): RegistrationFormState {
     return {
         usernameField: createTextFieldState(),
+        emailField: createTextFieldState(),
+        passwordField: createTextFieldState(),
         agreeCheckbox: createCheckboxState(),
     };
 }
@@ -43,11 +56,34 @@ const RegistrationForm: FC<RegistrationFormProps> = (props) => {
                 onChangeData={(data) => props.onChangeData({ ...props.data, usernameField: data })}
                 type="text"
                 maxLength={16}
+                variant="outlined"
+                margin="dense"
+                fullWidth
+            />
+            <TextField
+                label={t('pages.registerPage.email')}
+                data={props.data.emailField}
+                onChangeData={(data) => props.onChangeData({ ...props.data, emailField: data })}
+                type="text"
+                maxLength={191}
+                variant="outlined"
+                margin="dense"
+                fullWidth
+            />
+            <TextField
+                label={t('pages.registerPage.password')}
+                data={props.data.passwordField}
+                onChangeData={(data) => props.onChangeData({ ...props.data, passwordField: data })}
+                type="password"
+                variant="outlined"
+                margin="dense"
+                fullWidth
             />
             <Checkbox
                 label={termsAndConditionsLabel}
                 data={props.data.agreeCheckbox}
                 onChangeData={(data) => props.onChangeData({ ...props.data, agreeCheckbox: data })}
+                margin="dense"
             />
         </Form>
     );
@@ -58,6 +94,9 @@ export const RegisterPage: FC = () => {
     const [registrationForm, setRegistrationForm] = useState(createRegistrationFormState());
     return (
         <NavBarPage title={t('pages.registerPage.title')}>
+            <Typography component="h1" variant="h4">
+                {t('pages.registerPage.title')}
+            </Typography>
             <RegistrationForm data={registrationForm} onChangeData={(data) => setRegistrationForm(data)} />
         </NavBarPage>
     );
