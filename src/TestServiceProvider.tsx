223ccda1ce38082add26f34e011a9150f46596a1
inkognitro <contact@maecefischer.ch>
2022-06-20 14:41:11 +0200

    add theme

--- src/TestServiceProvider.tsx
+++ src/TestServiceProvider.tsx
@@ -8,6 +8,9 @@ import {
 import React, { FC, PropsWithChildren, useRef } from 'react';
 import { Config, ConfigProvider } from '@packages/core/config';
 import { MemoryRouter } from 'react-router-dom';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material';
+import { ThemeProvider as ScThemeProvider } from 'styled-components';
+import { theme } from '@components/theme';
 
 class StubCurrentUserRepository implements CurrentUserRepository {
     setCurrentUser(currentUser: AuthUser) {}
@@ -20,12 +23,16 @@ export const TestServiceProvider: FC<PropsWithChildren<{}>> = (props) => {
         companyName: 'ACME',
     });
     return (
-        <MemoryRouter>
-            <ConfigProvider value={configRef.current}>
-                <CurrentUserRepositoryProvider value={stubCurrentUserRepositoryRef.current}>
-                    <CurrentUserProvider value={anonymousAuthUser}>{props.children}</CurrentUserProvider>
-                </CurrentUserRepositoryProvider>
-            </ConfigProvider>
-        </MemoryRouter>
+        <MuiThemeProvider theme={theme}>
+            <ScThemeProvider theme={theme}>
+                <MemoryRouter>
+                    <ConfigProvider value={configRef.current}>
+                        <CurrentUserRepositoryProvider value={stubCurrentUserRepositoryRef.current}>
+                            <CurrentUserProvider value={anonymousAuthUser}>{props.children}</CurrentUserProvider>
+                        </CurrentUserRepositoryProvider>
+                    </ConfigProvider>
+                </MemoryRouter>
+            </ScThemeProvider>
+        </MuiThemeProvider>
     );
 };
